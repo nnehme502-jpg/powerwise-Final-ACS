2ec6328a1cdf157866673ee9870025e2b5f84b2d
nehme <nnehme502@gmail.com>
2026-05-11 01:37:20 +0300

    Remove backend_express_backup from repository

--- backend_express_backup/src/routes/simulation.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authMiddleware = require("../middleware/auth.middleware");
-const simulationController = require("../controllers/simulation.controller");
-
-router.post("/", authMiddleware, simulationController.runSimulation);
-
-module.exports = router;
